2bbbf7de78bb135e4a5e2d568457d86911a22b52
Aleksandr Poliakov <master@artwist.ru>
2023-10-10 21:20:06 +0300

    Реализовал слой хранения данных для коллекций

--- FakeNFT/UI/Catalog/Collections/NFTCollectionsDataSource.swift
+++ FakeNFT/UI/Catalog/Collections/NFTCollectionsDataSource.swift
@@ -1,8 +1,33 @@
-//
-//  NFTCollectionsDataSource.swift
-//  FakeNFT
-//
-//  Created by Александр Поляков on 10.10.2023.
-//
-
 import Foundation
+
+class NFTCollectionsDataSource {
+    private var collections: [CollectionModel]
+    
+    init(dataProvider: CatalogDataProviderProtocol) {
+        collections = dataProvider.giveMeAllCollections()
+    }
+    
+    func giveMeAllCollections() -> [CollectionModel] {
+        return collections
+    }
+    
+    func sortCollectionsByName(inOrder: SortCases = .ascending ) -> [CollectionModel] {
+        switch inOrder {
+        case .ascending:
+            return collections.sorted { $0.name < $1.name }
+        case .descending:
+            return collections.sorted { $0.name > $1.name }
+        }
+    }
+    
+    func sortCollectionsByNFTQuantity(inOrder: SortCases = .ascending) -> [CollectionModel] {
+        switch inOrder {
+        case .ascending:
+            return collections.sorted { $0.nfts.count < $1.nfts.count }
+        case .descending:
+            return collections.sorted { $0.nfts.count > $1.nfts.count }
+        }
+    }
+    
+    
+}
--- FakeNFT/UI/Catalog/Collections/SortCases.swift
+++ FakeNFT/UI/Catalog/Collections/SortCases.swift
@@ -1,8 +1,5 @@
-//
-//  sortCases.swift
-//  FakeNFT
-//
-//  Created by Александр Поляков on 10.10.2023.
-//
-
 import Foundation
+enum SortCases {
+    case ascending
+    case descending
+}
