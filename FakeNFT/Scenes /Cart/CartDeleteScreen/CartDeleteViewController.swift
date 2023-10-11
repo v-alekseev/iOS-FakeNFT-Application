08159a0d6c805223919588cc6785dea3bea58d1e
Vitaly Alekseev <valekseev@yandex.ru>
2023-10-11 16:19:44 +0300

    #25 Добавил первый вариант экрана удаления из корзины и механизм его вызова

---
+++ FakeNFT/Scenes /Cart/CartDeleteScreen/CartDeleteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CaerDeleteViewController.swift
+//  FakeNFT
+//
+//  Created by Vitaly on 11.10.2023.
+//
+
+import Foundation
+import UIKit
+
+final class CaerDeleteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ FakeNFT/Scenes /Cart/CartScreen/CartViewController+CartTableViewCellDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  CartViewController+CartTableViewCellDelegate.swift
+//  FakeNFT
+//
+//  Created by Vitaly on 11.10.2023.
+//
+
+import Foundation
